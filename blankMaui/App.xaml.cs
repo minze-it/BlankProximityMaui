a9a4e46145cf349ea0c92fc4cfd59505a440aeec
Markus Hinze <markus.hinze@audius.de>
2024-02-22 14:25:06 +0100

    Add project files.

---
+++ blankMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace blankMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ blankMaui/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Maui.Controls.PlatformConfiguration;
+
+namespace blankMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+
+#if WINDOWS
+            var _proximityDevice = Windows.Networking.Proximity.ProximityDevice.GetDefault();
+            var messageSubscriptionId = _proximityDevice.SubscribeForMessage("NDEF", (device, message) =>
+            {
+                System.Diagnostics.Debug.WriteLine("Hier");
+            });
+#endif
+        }
+    }
+
+}
